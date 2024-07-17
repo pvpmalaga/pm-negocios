b74ff41d6e6ba23cd8895a9faaab6c9c896a68da
pvpmalaga <82532161+pvpmalaga@users.noreply.github.com>
2024-07-18 01:00:50 +0200

    Update sv_main.lua

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -3,7 +3,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 QBCore.Functions.CreateCallback("trabajos:DB", function(source, cb)
     local src = source
     local trabajo = exports['origen_masterjob']:GetBusinesses()
-    local negocios = {}  -- Inicializar como una tabla vacía
+    local negocios = {}  
     
     for k, v in pairs(trabajo) do
         local data = v.Data
@@ -15,21 +15,20 @@ QBCore.Functions.CreateCallback("trabajos:DB", function(source, cb)
                 if coords then
                     local open_status = data.open == 1 and "true" or "false"
                     
-                    -- Crear un nuevo negocio como una tabla
+                  
                     local negocio = {
                         label = data.label,
                         open = open_status,
                         coords = { x = coords.x, y = coords.y, z = coords.z }
                     }
                     
-                    -- Agregar el negocio a la lista de negocios
                     table.insert(negocios, negocio)
                 end
             end
         end
     end
 
-    -- Enviar la lista de negocios al cliente
+    
     cb(negocios)
 end)
 
